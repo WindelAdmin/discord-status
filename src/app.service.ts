a78bd1698992b8819f19dcd3cadea43fe1b79a3e
Lucas Garcia <lucas.rita@windel.com.br>
2024-01-31 17:45:14 -0300

    FIX -

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,58 +1,48 @@
 import { Injectable } from '@nestjs/common';
-import CryptoJS from 'crypto-js';
 import { Client, GatewayIntentBits } from 'discord.js';
-
+var crypto = require('crypto');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 require('dotenv').config();
 
 @Injectable()
 export class AppService {
   async getWebhook(data: any) {
-    async function getToken() {
-      const passphrase = process.env.KEY_DECRYPT;
-      const tokenToDecrypt = process.env.TOKEN;
-
-      const tokenConvert = Buffer.from(tokenToDecrypt, 'base64').toString(
-        'utf-8',
-      );
-
-      const bytes = CryptoJS.AES.decrypt(tokenConvert, passphrase);
-
-      return await bytes.toString(CryptoJS.enc.Utf8);
-    }
-
-    client.on('ready', async () => {
-      const date = new Date();
-      console.log(
-        `Logged in as ${
-          client.user.tag
-        }! ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
-      );
-
-      const guild = client.guilds.cache.get(process.env.ID_SERVER);
-
-      const channel: any = guild.channels.cache.find(
-        (channel) => channel.name === 'back-commit',
-      );
-
-      // LIMPAR CANAL
-      // const channelWelcome = guild.channels.cache.find(
-      //   (channel) => channel.name === 'bem-vindo'
-      // );
-
-      // const messagess = await channelWelcome.messages.fetch({
-      //   limit: 40,
-      // });
-
-      // messagess.forEach(async (element) => {
-      //   await element.delete();
-      // });
-
-      if (data) {
-        channel.send(data);
+    try {
+      function getToken() {
+        return process.env.TOKEN;
       }
-    });
+      client.on('ready', async () => {
+        const date = new Date();
 
-    client.login(await getToken());
+        const guild = client.guilds.cache.get(process.env.ID_SERVER);
+        const channel: any = guild.channels.cache.find(
+          (channel) => channel.name === 'back-commit',
+        );
+
+        // LIMPAR CANAL
+        // const channelWelcome = guild.channels.cache.find(
+        //   (channel) => channel.name === 'bem-vindo'
+        // );
+
+        // const messagess = await channelWelcome.messages.fetch({
+        //   limit: 40,
+        // });
+
+        // messagess.forEach(async (element) => {
+        //   await element.delete();
+        // });
+
+        if (data) {
+          const message = `${data.commit.commit.message} - ${data.description}`;
+          channel.send(message);
+        }
+      });
+
+      const token = getToken();
+
+      client.login(token);
+    } catch (error) {
+      console.log('error: ', error);
+    }
   }
 }
