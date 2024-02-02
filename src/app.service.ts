53d1941426fc95096f86fcf0cb7dbad00d10c956
Lucas Garcia <lucks.garcialg@gmail.com>
2024-02-01 22:40:46 -0300

    7

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Client, GatewayIntentBits } from 'discord.js';
-var crypto = require('crypto');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-require('dotenv').config();
+import { client } from './client';
 
 @Injectable()
 export class AppService {
   async getWebhook(data: any) {
     try {
-      function getToken() {
-        return process.env.TOKEN;
-      }
-      client.on('ready', async () => {
+      if (client.isReady) {
         const guild = client.guilds.cache.get(process.env.ID_SERVER);
         const channel: any = guild.channels.cache.find(
           (channel) => channel.name === 'back-commit',
@@ -38,10 +32,7 @@ export class AppService {
           const message = `${mes} - ${desc}`;
           await channel.send(message);
         }
-      });
-
-      const token = getToken();
-      client.login(token);
+      }
     } catch (error) {
       console.log('error: ', error);
     }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { client } from './client';
 require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
+  await client.login(process.env.TOKEN);
 }
 bootstrap();
